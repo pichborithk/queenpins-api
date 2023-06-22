2e92f31c606e4700590df69e4377552abce7bd88
Pichborith Kong <pichborith@gmail.com>
2023-06-22 12:36:37 -0700

    handle create product route for admin user

--- src/routes/products.js
+++ src/routes/products.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const { getProducts } = require('../db/products');
+const { getProducts, createProduct } = require('../db/products');
+const deserializeUser = require('../middleware/deserializeUser');
 
 const router = express.Router();
 
@@ -14,6 +15,35 @@ router.get('/', async (req, res, next) => {
       data: {
         products,
       },
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post('/', deserializeUser, async (req, res, next) => {
+  if (req.user.type !== 'admin') {
+    return next({
+      name: 'AuthorizationHeaderError',
+      message: 'You must be an admin to perform this action',
+    });
+  }
+
+  try {
+    const { name, description, price, quantity, urls } = req.body;
+    const product = await createProduct({
+      name,
+      description,
+      price,
+      quantity,
+      urls,
+    });
+
+    res.status(200).json({
+      success: true,
+      error: null,
+      message: 'Success create new product',
+      data: product,
     });
   } catch (error) {
     next(error);
